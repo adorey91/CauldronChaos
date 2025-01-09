1704dd7d0d79e37737a0c5e928538c7e8fcdef3f
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-09 12:23:46 -0400

    Pickup Detection
    Pickups are now detected by a trigger volume attached to the player.

--- Assets/Scripts/Pickup/PickupBehviour.cs
+++ Assets/Scripts/Pickup/PickupBehviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupBehviour : MonoBehaviour
-{
+{ 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +15,5 @@ public class PickupBehviour : MonoBehaviour
     {
         
     }
+
 }
--- Assets/Scripts/Pickup/PickupObject.cs
+++ Assets/Scripts/Pickup/PickupObject.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class PickupObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isHeld = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
