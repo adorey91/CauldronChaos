b01e57585fb167e504959d72d7b49cf750f32400
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-09 10:21:27 -0400

    Created Input Actions
    Created the input actions for the player in gameplay.

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player Movement Variables")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Object references")]
+    [SerializeField] private CharacterController charController;
+
+    private Vector2 movementDir;
+
     // Start is called before the first frame update
     void Start()
     {
