777429683797a22703fe8dfde36f0670c9bbd61e
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-09 10:28:54 -0400

    Revert "Created Input Actions"
    This reverts commit b01e57585fb167e504959d72d7b49cf750f32400.

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Player Movement Variables")]
-    [SerializeField] private float moveSpeed = 5f;
-
-    [Header("Object references")]
-    [SerializeField] private CharacterController charController;
-
-    private Vector2 movementDir;
-
     // Start is called before the first frame update
     void Start()
     {
