57f93066f36ef43467683bb029137210f66af9b9
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-09 16:45:28 -0400

    Input Manager Rework Temporary
    Temporary Commit while reworking the input manager

--- Assets/Scripts/Pickup/PickupBehviour.cs
+++ Assets/Scripts/Pickup/PickupBehviour.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PickupBehviour : MonoBehaviour
 {
@@ -8,6 +10,26 @@ public class PickupBehviour : MonoBehaviour
     [SerializeField] private PickupDetection pickupVolume;
     [SerializeField] private Transform pickupHolder;
     private PickupObject heldObject = null;
+
+    //input requirements
+    private PlayerInputActions playerControls;
+    private InputAction pickup;
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        pickup = playerControls.Player.Pickup;
+        pickup.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pickup.Disable();
+    }
 
     // Update is called once per frame
     void Update()
@@ -37,4 +59,8 @@ public class PickupBehviour : MonoBehaviour
         }
     }
 
+    private void PickUpObject(InputAction.CallbackContext input)
+    {
+
+    }
 }
