b1003eb29c16424df8b6b1177e6964d717596f98
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-13 11:30:16 -0400

    Audio Manager  & SFX
    Created audio manager functionality relating to mixers. SFX manager also created.

--- Assets/Scripts/AudioSFXPlayer.cs
+++ Assets/Scripts/AudioSFXPlayer.cs
@@ -4,32 +4,29 @@ using UnityEngine;
 
 public enum SFX_Type
 {
-    PlayerHit,
-    PlayerShield,
-    PlayerShoot,
-    EnemyHit,
-    EnemyShield,
-    EnemyShoot,
-    Explosion,
+    Pickup,
+    Drop,
     UISounds
 }
 
 [System.Serializable]
 public class AudioSFXPlayer : MonoBehaviour
 {
-    public SFX_Type sfx_Type;
-    public AudioSource audioSource;
-    public float pitchVariance;
-    public float cooldownTime;
+    [SerializeField] public SFX_Type sfx_Type;
+    [SerializeField] public AudioSource audioSource;
+    [SerializeField] public float pitchVariance;
+    [SerializeField] private float cooldownTime;
+    [SerializeField] private AudioClip[] audioClips;
     private bool played;
+    private AudioClip lastPlayed = null;
 
     //Function to call audio SFX that does not inturrupt playing clip (can overlap)
-    public void PlayOneShot(AudioClip audioClip)
+    public void PlayOneShot()
     {
         if (!played)
         {
             audioSource.pitch = 1 + Random.Range(-pitchVariance, pitchVariance);
-            audioSource.PlayOneShot(audioClip);
+            audioSource.PlayOneShot(PickAudioClip());
             played = true;
 
             StartCoroutine(AudioCooldown());
@@ -37,10 +34,10 @@ public class AudioSFXPlayer : MonoBehaviour
     }
 
     //Function to call audio SFX that inturrupts playing clip
-    public void Play(AudioClip audioClip)
+    public void Play()
     {
-        audioSource.clip = audioClip;
         audioSource.pitch = 1 + Random.Range(-pitchVariance, pitchVariance);
+        audioSource.clip = PickAudioClip();
         audioSource.Play();
     }
 
@@ -49,5 +46,34 @@ public class AudioSFXPlayer : MonoBehaviour
     {
         yield return new WaitForSeconds(cooldownTime);
         played = false;
+    }
+
+    //function that picks an audioclip from the list of audio clips for the SFX player
+    private AudioClip PickAudioClip()
+    {
+        //clause statement for if the player has less than 1 audioclips
+        if (audioClips.Length <= 0) return null;
+
+        AudioClip audioClip = null;
+
+        //if more than one audio clip pick one that wasn't last played
+        if (audioClips.Length > 1)
+        {
+            //pick random audio clip
+            audioClip = audioClips[Random.Range(0, audioClips.Length -1)];
+
+            //if audio picked is the last one played pick a different one
+            while (audioClip == lastPlayed)
+            {
+                audioClip = audioClips[Random.Range(0, audioClips.Length - 1)];
+            }
+        }
+        //pick only audioclip
+        else
+        {
+            audioClip = audioClips[0];
+        }
+
+        return audioClip;
     }
 }
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,18 +1,151 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    public enum MixerGroup
+    {
+        Master,
+        Music,
+        SFX
+    }
+
+    [Header("Audio Managers")]
+    public SFXManager sfxManager;
+    public MusicManager musicManager;
+
+    [Header("Audio Mixers")]
+    [SerializeField] private AudioMixer masterMixer;
+
+    [Header("UI Elements")]
+    [SerializeField] private Slider[] masterSliders;
+    [SerializeField] private Slider[] musicSliders;
+    [SerializeField] private Slider[] sfxSliders;
+
+    [Header("Volume Settings")]
+    [SerializeField] private float initialVolume = 0.5f;
+
+    private static AudioManager _instance;
+
+    //function that checks if instance exists and spawns one if it does not
+    public static AudioManager instance
+    {
+        get
+        {
+            //check if instance is null
+            if (_instance == null)
+            {
+                //spawn instance
+                _instance = Instantiate(Resources.Load("AudioManager") as GameObject).GetComponent<AudioManager>();
+                _instance.name = "InputManager"; //renames the game object to InputManager
+            }
+            return _instance; //returns 
+        }
+    }
+
+    // Awake is called before the first frame update and before start
+    void Awake()
+    {
+        //check if this is the active instance
+        if (!_instance || _instance == this)
+        {
+            _instance = this;
+        }
+        else
+        {
+            //remove copy
+            Destroy(gameObject);
+        }
+
+        //add to don't destroy on load
+        DontDestroyOnLoad(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeMixer(MixerGroup.Master);
+        InitializeMixer(MixerGroup.Music);
+        InitializeMixer(MixerGroup.SFX);
     }
 
-    // Update is called once per frame
-    void Update()
+    //function that sets volume of mixers
+    public void SetVolume(MixerGroup target, float volume)
     {
-        
+        switch (target)
+        {
+            case MixerGroup.Master:
+                masterMixer.SetFloat("MasterVolume", ConvertToDB(volume)); //replace string with 
+                break;
+
+            case MixerGroup.Music:
+                masterMixer.SetFloat("MusicVolume", ConvertToDB(volume));
+                break;
+
+            case MixerGroup.SFX:
+                masterMixer.SetFloat("SFXVolume", ConvertToDB(volume));
+                break;
+        }
+    }
+
+    public void SetMasterVolume(float sliderValue)
+    {
+        SetVolume(MixerGroup.Master, sliderValue);
+    }
+
+    public void SetSFXVolume(float sliderValue)
+    {
+        SetVolume(MixerGroup.SFX, sliderValue);
+    }
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        SetVolume(MixerGroup.Music, sliderValue);
+    }
+
+    //utility function for converting linear float to DB
+    private float ConvertToDB(float value)
+    {
+        return Mathf.Log10(value) * 20f;
+    }
+
+    private void InitializeMixer(MixerGroup mixerGroup)
+    {
+        switch (mixerGroup)
+        {
+            case MixerGroup.Master:
+                masterMixer.SetFloat("MasterVolume", ConvertToDB(initialVolume));
+
+                //setting master sliders to initial volume
+                for (int i = 0; i < masterSliders.Length; i++)
+                {
+                    masterSliders[i].value = initialVolume;
+                }
+                break;
+
+            case MixerGroup.Music:
+                masterMixer.SetFloat("MusicVolume", ConvertToDB(initialVolume));
+
+                //setting music sliders to initial volume
+                for (int i = 0; i < musicSliders.Length; i++)
+                {
+                    musicSliders[i].value = initialVolume;
+                }
+                break;
+
+            case MixerGroup.SFX:
+                masterMixer.SetFloat("SFXVolume", ConvertToDB(initialVolume));
+
+                //setting sfx sliders to initial volume
+                for (int i = 0; i < sfxSliders.Length; i++)
+                {
+                    sfxSliders[i].value = initialVolume;
+                }
+                break;
+        }
     }
 }
