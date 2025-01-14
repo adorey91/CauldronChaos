744af8385516d81cd88b5da3217bf3783f05a51a
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-14 09:39:47 -0400

    AudioSFX rework
    Reworked AudioSFXPlayer to receive sound clips so that not all sound effects need to be loaded into memory at once. Moved the picking sound functionality into its own script.

---
+++ Assets/Scripts/Audio/SFXLibrary.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXLibrary : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;
+    private AudioClip lastPlayed = null;
+
+    //function that picks an audioclip from the list of audio clips for the SFX player
+    public AudioClip PickAudioClip()
+    {
+        //clause statement for if the player has less than 1 audioclips
+        if (audioClips.Length <= 0) return null;
+
+        AudioClip audioClip = null;
+
+        //if more than one audio clip pick one that wasn't last played
+        if (audioClips.Length > 1)
+        {
+            //pick random audio clip
+            audioClip = audioClips[Random.Range(0, audioClips.Length - 1)];
+
+            //if audio picked is the last one played pick a different one
+            while (audioClip == lastPlayed)
+            {
+                audioClip = audioClips[Random.Range(0, audioClips.Length - 1)];
+            }
+        }
+        //pick only audioclip
+        else
+        {
+            audioClip = audioClips[0];
+        }
+
+        return audioClip;
+    }
+}
