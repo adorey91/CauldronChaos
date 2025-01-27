12a4a1a16b03313e307cd8a60bae573b0317069d
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-27 12:18:41 -0400

    Fixed player collisions
    modified the player movement code to better detect collisions with obstacles.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 180;
 
+    [Header("Collision Detection")]
+    [SerializeField] private Transform castPos; //position to do raycasts from
+    [SerializeField] private float playerRadius; //radius of the player collision collider
+    [SerializeField] private LayerMask collisionsLayers; //layers on which collisions happen
+
     [Header("Object References")]
     [SerializeField] private Rigidbody playerRB;
 
@@ -44,18 +49,57 @@ public class PlayerMovement : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
         }
-        
 
         //apply multipliers
         movement *= moveSpeed * Time.fixedDeltaTime;
+
+        //handle collision detection
+        movement = CheckMove(movement);
 
         //apply movement
         playerRB.MovePosition(playerRB.position + movement);
     }
 
+
     private void GetMove(InputAction.CallbackContext input)
     {
         moveDir = input.ReadValue<Vector2>();
         //Debug.Log("Get Move being called");
+    }
+
+    //Function that tries to detect any collisions and modify the move to account for them
+    private Vector3 CheckMove(Vector3 move)
+    {
+        Vector3 legalMove = Vector3.zero; //varaible to store what version of the 
+
+        //do initial check to see if move will collide with anything
+        if (DetectCollisions(move))
+        {
+            //check move that uses only x component
+            legalMove = new Vector3(move.x, 0f, 0f);
+            if (DetectCollisions(legalMove))
+            {
+                //check move that uses only z component
+                legalMove = new Vector3(0f, 0f, move.z);
+                if (DetectCollisions(legalMove))
+                {
+                    //if collision is detected for both directions movement is stopped
+                    legalMove = Vector3.zero;
+                }
+            }
+        }
+        //allow original move
+        else
+        {
+            legalMove = move; 
+        }
+
+        return legalMove;
+    }
+
+    //Function that checks if the player will collide with anything during their move
+    private bool DetectCollisions(Vector3 move)
+    {
+        return Physics.Raycast(castPos.position, move.normalized, move.magnitude + playerRadius, collisionsLayers);
     }
 }
