9dddf546ae8120de4b059d73d9f0f0c308f7a0ee
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-28 19:55:06 -0400

    Player Movement changes
    Removed tertiary start function.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -31,12 +31,6 @@ public class PlayerMovement : MonoBehaviour
         InputManager.instance.MoveAction -= GetMove;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
