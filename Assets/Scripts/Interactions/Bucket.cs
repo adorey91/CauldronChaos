ec6e540f3dc90c892ff41ad5c8c8f4234780c0ac
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-30 14:20:48 -0400

    New Interaction System Testing
    Tested to see if the new interaction system was working correctly.

---
+++ Assets/Scripts/Interactions/Bucket.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : Interactable
+{
+    public override void Interact()
+    {
+        Debug.Log("Bucket interacted with");
+    }
+}
