ec6f24f67b520aad9c1c1cb32617f1fee7f9a5af
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-02-03 21:13:36 -0400

    Broom Rework
    Reworked the broom to use animations rather than controlling rotation through code.

--- Assets/Scripts/Interactions/Broom.cs
+++ Assets/Scripts/Interactions/Broom.cs
@@ -6,16 +6,15 @@ using UnityEngine;
 public class Broom : Interactable
 {
     [Header("Broom Variables")]
+    [SerializeField] private LayerMask interactionLayers;//layers that interact with the broom effect
     [SerializeField] private Transform impactPoint; //point at which the explosion force of the broom will be applied
+    [SerializeField] private Animator animator; //animator used to make the broom movement
     [SerializeField] private float moveForce; //force applied to physics objects near the impact point
     [SerializeField] private float forceRadius; //radius in which the force will be applied
-    [SerializeField] private float swingAngle; //angle which the swing reaches
-    [SerializeField] private float swingTime; //total time spent in swing animation
-    [SerializeField][Range(0.1f, 0.5f)] private float swingRatio; //ratio between time into swing to time out of swing
 
     public override void Interact()
     {
-        StartCoroutine(SwingAnim());
+        animator.Play("BroomHit");
     }
 
     public override void Interact(PickupBehaviour pickup)
@@ -24,9 +23,10 @@ public class Broom : Interactable
     }
 
     //Method that handles the functionality for the broom swing
-    private void SwingFunc()
+    public void SwingFunc()
     {
-        Collider[] colliders = Physics.OverlapSphere(impactPoint.position, forceRadius); //get any colliders in the area
+
+        Collider[] colliders = Physics.OverlapSphere(impactPoint.position, forceRadius, interactionLayers); //get any colliders in the area
         //Debug.Log(colliders.Length);
 
         //loop through and apply force to nearby rigid bodies
@@ -38,58 +38,5 @@ public class Broom : Interactable
                 rb.AddExplosionForce(moveForce, impactPoint.position, forceRadius); //apply the explosion "cleaning" force
             }
         }
-    }
-
-    //Coroutine that preforms an animation through code an applies force for "cleaning"
-    private IEnumerator SwingAnim()
-    {
-        float rotation = 0f;//current rotated angle
-        float lastRotation = 0f;//last rotated angle
-        float requiredRotation;//rotation required to reach current rotations
-        float timer = 0f;
-        float inTime = swingTime * swingRatio; //calculating swing in time
-        float outTime = swingTime * (1 - swingRatio); //calculation swing out time
-
-        //swing in
-        while (timer < inTime)
-        {
-            lastRotation = rotation;
-            rotation = Mathf.Lerp(0, swingAngle, Mathf.Pow(timer/inTime, 2)); //calculate angle in swing along exponential curve
-            requiredRotation = rotation - lastRotation; //calculate required roation to reach current angle
-            Debug.Log(rotation);
-
-            transform.parent.RotateAround(transform.parent.position, transform.parent.right, requiredRotation); //apply rotations
-
-            yield return null;
-            timer += Time.deltaTime; //increment timer
-        }
-
-        //snap to correct value
-        //transform.parent.localEulerAngles = transform.parent.forward * swingAngle;
-
-        //apply swing functionality
-        SwingFunc();
-
-        //resetting varaibles
-        timer = 0f; 
-        rotation = 0f;
-        lastRotation = 0f;
-
-        //swing out
-        while (timer < outTime)
-        {
-            lastRotation = rotation;
-            rotation = -Mathf.SmoothStep(swingAngle, 0, timer/outTime); //calculate angle out of swing with damping
-            requiredRotation = rotation - lastRotation; //calculate required roation to reach current angle
-            Debug.Log(rotation);
-
-            transform.parent.RotateAround(transform.parent.position, transform.parent.right, requiredRotation); //apply rotations
-
-            yield return null;
-            timer += Time.deltaTime; //increment timer
-        }
-
-        //snap to correct value
-        //transform.parent.RotateAround;
     }
 }
